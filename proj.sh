3f5a4e498b1813055dc4146c9d046baf6127b544
nduvalhall <nduvalhall@gmail.com>
2024-01-11 19:43:31 +0000

    removed use of -i for sed for compatibility

--- proj.sh
+++ proj.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+
+
 function proj() {
+
+    # Check if file exists, if not create it
     file_path="/home/$(whoami)/.local/share/proj/.config"
+    
+    if [[ ! -f $file_path ]]; then
+        mkdir -p /home/$(whoami)/.local/share/proj
+        touch $file_path
+    fi
+
     file=$(cat $file_path)
 
 
+    # if no arguments are passed, show help
     if [[ $# -eq 0 ]]; then
         echo "usage: proj [options] [project name]"
         echo "options:"
@@ -16,12 +28,14 @@ function proj() {
     fi
 
 
+    # if first argument is --version or -v, show version
     if [[ $1 == "--version" || $1 == "-v" ]]; then
         echo "proj version 0.2"
         return 0
     fi
 
 
+    # if first argument is --help or -h, show help
     if [[ $1 == "--help" || $1 == "-h" ]]; then
         echo "usage: proj [options] [project name]"
         echo "options:"
@@ -35,20 +49,33 @@ function proj() {
     fi
 
 
+    # if first argument is --list or -l, list all projects
     if [[ $1 == "--list" || $1 == "-l" ]]; then
+
+        # check if file is empty
+        if [[ ! -s "$file_path" ]]; then
+            echo "No projects found, use 'proj --add' to add a project"
+            return 1
+        fi
+
+        # list all projects
         cat $file_path
         return 0
     fi
 
 
+    # if first argument is --add or -a, add project
     if [[ $1 == "--add" || $1 == "-a" ]]; then
 
 
+        # if no arguments are passed, show help
         if [[ $2 == "" || $3 == "" ]]; then
             echo "usage: proj --add [project name] [project path]"
             return 1
 
         else
+
+            # check if project already exists
             count=$(grep -c "$2 " $file_path)
 
             if [[ $count -gt 0 ]]; then
@@ -56,6 +83,7 @@ function proj() {
                 return 1
             fi
 
+            # add project
             name=$2
             path=$(realpath $3)
             echo "$name $path" >> $file_path
@@ -65,13 +93,17 @@ function proj() {
     fi
 
 
+    # if first argument is --remove or -r, remove project
     if [[ $1 == "--remove" || $1 == "-r" ]]; then
 
+        # if no arguments are passed, show help
         if [[ $2 == "" ]]; then
             echo "usage: proj --remove [project name]"
             return 1
 
         else
+
+            # check if project exists
             count=$(grep -c "$2 " $file_path)
 
             if [[ $count -eq 0 ]]; then
@@ -79,20 +111,28 @@ function proj() {
                 return 1
             fi
 
+            # remove project
             name=$2
-            sed -i "/$name /d" $file_path
+            after=$(sed "/$name /d" $file_path)
+            echo "$after" > $file_path
+
             echo "Removed project '$name'"
             return 0
         fi
     fi
 
+
+    # if first argument is --open or -o, open project
     if [[ $1 == "--open" || $1 == "-o" ]]; then
 
+        # if no arguments are passed, show help
         if [[ $2 == "" ]]; then
             echo "usage: proj --open [project name]"
             return 1
 
         else
+
+            # check if project exists
             count=$(grep -c "$2 " $file_path)
 
             if [[ $count -eq 0 ]]; then
@@ -100,6 +140,7 @@ function proj() {
                 return 1
             fi
 
+            # open project
             name=$2
             path=$(grep "$name " $file_path | cut -d' ' -f2)
             cd $path

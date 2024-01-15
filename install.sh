48c63c4d29f4b3b9b92f556b4ec2c893d15c80e7
nduvalhall <nduvalhall@gmail.com>
2024-01-15 20:44:40 +0000

    updated install script to install fzf

--- install.sh
+++ install.sh
@@ -3,6 +3,21 @@
 # copy proj.sh to /usr/local/bin
 sudo cp ./proj.sh /usr/local/bin/
 
+# install fzf
+apt=$(which apt)
+dnf=$(which dnf)
+
+if [ -n "$apt" ]; then
+    sudo apt install fzf
+elif [ -n "$dnf" ]; then
+    sudo dnf install fzf
+else
+    echo "Error: no package manager found"
+    exit 1
+fi
+
+
+
 # add proj.sh to ~/.bashrc
 echo "" >> ~/.bashrc
 echo "#used for proj tool" >> ~/.bashrc

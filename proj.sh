8f0dedad6db0893138259d07b85ed4580548aa8c
nduvalhall <nduvalhall@gmail.com>
2024-03-30 11:50:35 +0000

    created add and remove functionality

--- proj.sh
+++ proj.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-
 function proj() {
-    file_path=$(find ~/* -type d | fzf --reverse)
-    if [[ -z "$file_path" ]]; then
-        return
-    fi
-    cd $file_path
+	proj_path=$HOME/.local/share/proj
+	config_file=$proj_path/config
+
+	if [ ! -f $config_file ]; then
+		if [ ! -d $proj_path ]; then
+			mkdir -p $proj_path
+		fi
+		touch $config_file
+	fi
+
+	if [[ $1 == "add" || $1 == "a" ]]; then
+		echo $(pwd) >>$config_file
+		echo "Added $(pwd) to proj"
+		return
+	fi
+
+	if [[ $1 == "remove" || $1 == "r" ]]; then
+		file_path=$(cat $config_file | fzf --reverse)
+		touch $proj_path/temp
+
+		while read -r line; do
+			if [ "$line" != "$file_path" ]; then
+				echo $line >>$proj_path/temp
+			fi
+		done <$config_file
+
+		mv $proj_path/temp $config_file
+		echo "Removed $file_path from proj"
+		return
+	fi
+
+	file_path=$(cat $config_file | fzf --reverse)
+
+	if [[ $file_path == "" ]]; then
+		return
+	fi
+
+	cd $file_path
+	clear
 }
